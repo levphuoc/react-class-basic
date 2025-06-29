110366b66a76dcb6cb9be08dbd0da226cedf500a
levphuoc <lephuongphuoc15@gmail.com>
2025-06-29 18:11:59 +0700

    overview create a state and change value from state

--- react-class/src/views/Example/MyComponent.js
+++ react-class/src/views/Example/MyComponent.js
@@ -1,13 +1,34 @@
-import React from "react";  
+import React, { Fragment } from "react";  
 
 class MyComponent extends React.Component
 {
-    render()
-    {
+    state = {
+        name: 'Phuoc',
+        channel: 'Phuoc dep trai'
+    }
+    /* 
+    JSX => return block
+    */
+   handleOnChangeName = (event) =>{
+    this.setState({
+        name: event.target.value
+    })
+   }
+    render(){
+        
         return(
-            <div>
-                Hello my components
+            <Fragment>
+            <div className="abc">
+                <input value={this.state.name} type="text"
+                onChange={(event) => this.handleOnChangeName(event)} 
+                />
+                {/* Hello, My name is : {this.state.name} */}
+                Hello, My name is : {this.state['name']}
             </div>
+            <div  className="second">
+                My Facebook channel is : {this.state.channel}
+            </div>
+            </Fragment>
         );
     }
 }
